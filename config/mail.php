fa0a7220c0557481f0211084bc29ca9baed66ac5
Woid <71274141+WoidZero@users.noreply.github.com>
2021-12-06 16:33:12 +0300

    Add files via upload

---
+++ config/mail.php
@@ -0,0 +1,9 @@
+<?php
+$smtp = ""; //smtp server adress
+$mail_server = ""; //mail adress
+$mail_server_password = ""; //mail password
+$mail_type = 'ssl'; // choose ssl or tls
+$smtp_port = 465; //smtp port
+$url_register = "DOMAIN/tools/activate.php";
+$url_reset = "DOMAIN/tools/reset.php"; //enter url to your activate account file. Example: https://example.com/database/activate.php
+?>
